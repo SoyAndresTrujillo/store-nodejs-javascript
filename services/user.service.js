3b1fe3680bbb6f30bf37d23c14f411ae2e5868ed
soyandrestrujillo <andymania.1301@gmail.com>
2024-04-12 08:03:17 -0500

    init postgres orm and created model user

--- services/user.service.js
+++ services/user.service.js
@@ -1,19 +1,15 @@
-const pool = require('../lib/postgres');
+const { models } = require('../lib/sequelize');
 
 class UserService {
-  constructor() {
-    this.pool = pool,
-    this.pool.on('error', (error) => console.error(error))
-  }
+  constructor() {}
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const response = await this.pool.query(query);
-    return response.rows;
+    const response = await models.User.findAll();
+    return response;
   }
 
   async findOne(id) {
